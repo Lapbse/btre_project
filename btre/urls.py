128ebd3b1480a3a9cabc5e4aefb5bafe6daa757d
Lapbse <lapbse@gmail.com>
2020-02-16 00:32:05 -0500

    Perfecto Video 30 Terminado Listings

--- btre/urls.py
+++ btre/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('pages.urls')), #REFERENCIA A TODOS LOS URLS DEL PROYECTO
+    path('', include('pages.urls')), #Referencia a los urls de la app pages
+    path('listings/', include('listings.urls')), #Referencia a los urls de la app listings
     path('admin/', admin.site.urls), #POR DEFECTO
 ]
