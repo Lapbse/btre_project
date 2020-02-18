1271307878dbcbe92d6909d3bb7079a816992204
Lapbse <lapbse@gmail.com>
2020-02-17 20:13:37 -0500

    V_39_Ok Customize Admin Display Data

--- btre/urls.py
+++ btre/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('', include('pages.urls')), #Referencia a los urls de la app pages
     path('listings/', include('listings.urls')), #Referencia a los urls de la app listings
     path('admin/', admin.site.urls), #POR DEFECTO
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_URL)
+
