42ace6733a6901ca91b4da712a591ccd418a92a6
Lapbse <lapbse@gmail.com>
2020-02-21 12:56:50 -0500

    V_44_Ok Listing Dynamic Content

--- listings/views.py
+++ listings/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Listing
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+
 
 
 def index(request):
@@ -10,7 +11,6 @@ def index(request):
     page = request.GET.get('page')
     paged_listings = paginator.get_page(page)
 
-
     context = {
         'listings': paged_listings
     }
@@ -19,7 +19,14 @@ def index(request):
 
 
 def listing(request, listing_id):
-    return render(request, 'listings/listing.html')
+    listing = get_object_or_404(Listing, pk=listing_id)
+
+    context = {
+        'listing':listing
+    }
+
+    return render(request, 'listings/listing.html', context)
+
 
 
 def search(request):
