6681d5bd5ee23ac26d32ba60c7ec1f4d1b2ab885
Lapbse <lapbse@gmail.com>
2020-03-18 16:29:39 -0400

    V_50 OK_Register Mejorado Terminado_Solo falta que Desaparezca solo el Mensaje Rojo de Error

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
+
 
 def register(request):
     if request.method == 'POST': #Si procesó el formulario...
-        # Register user
-        return
+        messages.error(request, 'Testing error message')
+        return redirect('register')
     else:
         return render(request, 'accounts/register.html')
 
