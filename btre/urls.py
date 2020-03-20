56f3ec07cafafbd0688bc589123bd00f696a1a6b
Lapbse <lapbse@gmail.com>
2020-03-20 07:23:45 -0400

    V_58 OK  - Contact Form Submission

--- btre/urls.py
+++ btre/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('', include('pages.urls')), #Referencia a los urls de la app pages
     path('listings/', include('listings.urls')), #Referencia a los urls de la app listings
     path('accounts/', include('accounts.urls')), #Referencia a los urls de la app accounts
+    path('contacts/', include('contacts.urls')), #Referencia a los urls de la app contact
     path('admin/', admin.site.urls), #POR DEFECTO
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
