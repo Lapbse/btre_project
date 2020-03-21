6d2a37fd67e987d3d50ad3f749b419d199420fa0
Lapbse <lapbse@gmail.com>
2020-03-21 17:01:50 -0400

    Added localsettings

--- btre/settings.py
+++ btre/settings.py
@@ -153,3 +153,8 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'info@pelanda.es'
 EMAIL_HOST_PASSWORD = 'Leo.06101973'
 EMAIL_USE_TLS = True
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
